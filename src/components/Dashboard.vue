e5b51b149ebf1bf0f322336eaf459ad8aaa788be
Ackerman-D-Edward <ackermandedward@gmail.com>
2024-03-25 00:19:01 +0100

    adicionado mensagens nas atualizações e remoções de pedidos e final do projeto

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="burger-table">
+        <Message :msg="msg" v-show="msg" />
         <div>
             <div id="burger-table-heading">
                 <div class="order-id">#:</div>
@@ -39,14 +40,20 @@
 </template>
 
 <script>
+import Message from './Message.vue';
+
 export default {
     name: "Dashboard",
     data() {
         return {
             burgers: null,
             burger_id: null,
-            status: []
+            status: [],
+            msg: null
         }
+    },
+    components: {
+        Message
     },
     methods: {
         async getPedidos() {
@@ -80,7 +87,11 @@ export default {
 
             const res = await req.json();
 
-            // msg
+            // colocar uma msg de sistema
+            this.msg = `Pedido removido com sucesso!`;
+
+            // limpar msg
+            setTimeout(() => this.msg = "", 3000);
 
             this.getPedidos();
 
@@ -98,6 +109,12 @@ export default {
             });
 
             const res = await req.json();
+
+            // colocar uma msg de sistema
+            this.msg = `Pedido Nº ${res.id} foi atualizado para ${res.status}!`;
+
+            // limpar msg
+            setTimeout(() => this.msg = "", 3000);
 
             console.log(res);
 
