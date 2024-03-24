086f11b9b4d8898eba387f97f321fd3750b48383
Ackerman-D-Edward <ackermandedward@gmail.com>
2024-03-24 16:59:57 +0100

    inserindo os dados do pedido no banco

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Componente de Mensagem</p>
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit="createBurger">
                 <div class="input-container">
                     <label for="nome">Nome do Cliente:</label>
                     <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
@@ -54,7 +54,6 @@ export default {
             pao: null,
             carne: null,
             opcionais: [],
-            status: "Solicitado",
             msg: null
         }
     },
@@ -67,6 +66,39 @@ export default {
             this.paes = data.paes;
             this.carnes = data.carnes;
             this.opcionaisdata = data.opcionais;
+
+        },
+        async createBurger(e) {
+
+            e.preventDefault();
+
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opicionais: Array.from(this.opcionais),
+                status: "Solicitado"
+            }
+
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch("http://localhost:3000/burgers", {
+                method: "POST",
+                headers: { "Content-Type": "aplication/json" },
+                body: dataJson
+            });
+
+            const res = await req.json();
+
+            // colocar uma msg de sistema
+
+            // limpar msg
+
+            // limpar os campos
+            this.nome = "";
+            this.carne = "";
+            this.pao = "";
+            this.opcionais = "";
 
         }
     },
